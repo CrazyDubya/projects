ba757d8eb49f313869e880aca5779749c7fa61c6
Puppuccino <97849040+CrazyDubya@users.noreply.github.com>
2024-05-15 22:12:38 -0400

    Update autopy.py

--- iPhone toss to Mac/autopy.py
+++ iPhone toss to Mac/autopy.py
@@ -16,7 +16,7 @@ class NewFileHandler(FileSystemEventHandler):
                                        universal_newlines=True)
 
             # Create the console log file
-            console_log_path = '/Users/puppuccino/PycharmProjects/inner_mon/Charmed//console30/console_log.txt'
+            console_log_path = '/Path/console_log.txt'
             os.makedirs(os.path.dirname(console_log_path), exist_ok=True)
 
             # Read and print the console output in real-time
@@ -41,7 +41,7 @@ class NewFileHandler(FileSystemEventHandler):
 
 
 if __name__ == "__main__":
-    path = '/Users/puppuccino/PycharmProjects/inner_mon/Charmed/charm'
+    path = '/PATH/charm
     event_handler = NewFileHandler()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=False)
